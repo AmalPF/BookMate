1391cb1f47d6cc2eaa9b14f3fc80324188677b03
Abhinav Krishna <abhinav.krishna@soti.net>
2022-10-07 15:31:24 +0530

    Controllers added- admin,purchase,cart

---
+++ BookMate/Controllers/CartsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BookMate.Models;
+
+namespace BookMate.Controllers
+{
+    public class CartsController : Controller
+    {
+        private BookMateDBEntities db = new BookMateDBEntities();
+
+        // GET: Carts
+        public ActionResult Index()
+        {
+            var cart = db.Cart.Include(c => c.Books).Include(c => c.Users);
+            return View(cart.ToList());
+        }
+
+        // GET: Carts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cart cart = db.Cart.Find(id);
+            if (cart == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cart);
+        }
+
+        // GET: Carts/Create
+        public ActionResult Create()
+        {
+            ViewBag.BId = new SelectList(db.Books, "BId", "BName");
+            ViewBag.UId = new SelectList(db.Users, "UId", "UUserName");
+            return View();
+        }
+
+        // POST: Carts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CId,UId,BId")] Cart cart)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cart.Add(cart);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.BId = new SelectList(db.Books, "BId", "BName", cart.BId);
+            ViewBag.UId = new SelectList(db.Users, "UId", "UUserName", cart.UId);
+            return View(cart);
+        }
+
+        // GET: Carts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cart cart = db.Cart.Find(id);
+            if (cart == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.BId = new SelectList(db.Books, "BId", "BName", cart.BId);
+            ViewBag.UId = new SelectList(db.Users, "UId", "UUserName", cart.UId);
+            return View(cart);
+        }
+
+        // POST: Carts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CId,UId,BId")] Cart cart)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cart).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.BId = new SelectList(db.Books, "BId", "BName", cart.BId);
+            ViewBag.UId = new SelectList(db.Users, "UId", "UUserName", cart.UId);
+            return View(cart);
+        }
+
+        // GET: Carts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cart cart = db.Cart.Find(id);
+            if (cart == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cart);
+        }
+
+        // POST: Carts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cart cart = db.Cart.Find(id);
+            db.Cart.Remove(cart);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BookMate/Controllers/PurchasesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BookMate.Models;
+
+namespace BookMate.Controllers
+{
+    public class PurchasesController : Controller
+    {
+        private BookMateDBEntities db = new BookMateDBEntities();
+
+        // GET: Purchases
+        public ActionResult Index()
+        {
+            var purchase = db.Purchase.Include(p => p.Address).Include(p => p.Books);
+            return View(purchase.ToList());
+        }
+
+        // GET: Purchases/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Purchase purchase = db.Purchase.Find(id);
+            if (purchase == null)
+            {
+                return HttpNotFound();
+            }
+            return View(purchase);
+        }
+
+        // GET: Purchases/Create
+        public ActionResult Create()
+        {
+            ViewBag.AId = new SelectList(db.Address, "AId", "AAddressL1");
+            ViewBag.BId = new SelectList(db.Books, "BId", "BName");
+            return View();
+        }
+
+        // POST: Purchases/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PId,BId,AId,PDateTime,PQuantity,PAmount")] Purchase purchase)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Purchase.Add(purchase);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AId = new SelectList(db.Address, "AId", "AAddressL1", purchase.AId);
+            ViewBag.BId = new SelectList(db.Books, "BId", "BName", purchase.BId);
+            return View(purchase);
+        }
+
+        // GET: Purchases/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Purchase purchase = db.Purchase.Find(id);
+            if (purchase == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AId = new SelectList(db.Address, "AId", "AAddressL1", purchase.AId);
+            ViewBag.BId = new SelectList(db.Books, "BId", "BName", purchase.BId);
+            return View(purchase);
+        }
+
+        // POST: Purchases/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PId,BId,AId,PDateTime,PQuantity,PAmount")] Purchase purchase)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(purchase).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AId = new SelectList(db.Address, "AId", "AAddressL1", purchase.AId);
+            ViewBag.BId = new SelectList(db.Books, "BId", "BName", purchase.BId);
+            return View(purchase);
+        }
+
+        // GET: Purchases/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Purchase purchase = db.Purchase.Find(id);
+            if (purchase == null)
+            {
+                return HttpNotFound();
+            }
+            return View(purchase);
+        }
+
+        // POST: Purchases/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Purchase purchase = db.Purchase.Find(id);
+            db.Purchase.Remove(purchase);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
